cbacf72085e1b6dd95b2900af3f170d4fb079266
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-04-07 07:52:00 +0600

    main jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -13,11 +13,17 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { AuthProvider } from '../context/AuthContext.jsx'
+import { CartProvider } from '../context/CartContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>
 )
